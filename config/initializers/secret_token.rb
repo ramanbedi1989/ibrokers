3b54f4afe54c3eba82ab56c9e2c3e6c385d23b8b
ramanbedi1989 <ramanbedi1989@gmail.com>
2014-02-26 20:53:58 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ibrokers::Application.config.secret_key_base = '56c15d884212793f44cfd87814bcbfadaea691b1d091dfaa5857a0aa61eac50ed3d93209739bc549da1197ad6d5db0592cc3abac4f0d8aaa584732d5371b3a99'
