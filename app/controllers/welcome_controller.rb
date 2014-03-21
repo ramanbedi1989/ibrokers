36c08c84a7c84e63b9c543e22a72c55ac891c1e2
ramanbedi1989 <ramanbedi1989@gmail.com>
2014-03-21 23:06:37 +0530

    change content

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   	@slider_categories = [
-  		{image: 'assets/carousel-image.png',title: 'Welcome to Ibrokers' , description: "Our website has changed, but our commitment to you never will.", url: '#'},
-  		{image: 'assets/carousel-image.png',title: 'Welcome to Ibrokers' , description: 'Our website has changed, but our commitment to you never will.', url: '#'},
-  		{image: 'assets/carousel-image.png',title: 'Welcome to Ibrokers' , description: 'Our website has changed, but our commitment to you never will.', url: '#'},
-  		{image: 'assets/carousel-image.png',title: 'Welcome to Ibrokers' , description: 'Our website has changed, but our commitment to you never will.', url: '#'}
+  		{image: 'assets/carousel-image.png',title: 'Adviser Net' , description: "Our website has changed, but our commitment to you never will.", url: '#'},
+  		{image: 'assets/carousel-image.png',title: 'acc cover + extra' , description: 'Our website has changed, but our commitment to you never will.', url: '#'},
+  		{image: 'assets/carousel-image.png',title: 'business and house insurance quote' , description: 'Our website has changed, but our commitment to you never will.', url: '#'},
+  		{image: 'assets/carousel-image.png',title: 'win a trip to fiji' , description: 'Our website has changed, but our commitment to you never will.', url: '#'}
   	]
   end
 end
