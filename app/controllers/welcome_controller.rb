1da67c80d2ce7d446704a321db44f318c5120e6e
navnitrai <rai.navnit@gmail.com>
2014-05-30 11:22:58 +0530

    changed slider image

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,9 @@ class WelcomeController < ApplicationController
   def index
   	@slider_categories = [
   		{image: 'assets/carousel-image.png',title: 'Adviser Net' , description: "Our website has changed, but our commitment to you never will.", url: '#'},
-  		{image: 'assets/carousel-image.png',title: 'acc cover + extra' , description: 'Our website has changed, but our commitment to you never will.', url: '#'},
+  		{image: 'assets/Protection-2.jpg',title: 'acc cover + extra' , description: 'Our website has changed, but our commitment to you never will.', url: '#'},
   		{image: 'assets/carousel-image.png',title: 'business and house insurance quote' , description: 'Our website has changed, but our commitment to you never will.', url: '#'},
-  		{image: 'assets/carousel-image.png',title: 'win a trip to fiji' , description: 'Our website has changed, but our commitment to you never will.', url: '#'}
+  		{image: 'assets/Music Carnival.jpg',title: 'win a trip to fiji' , description: 'Our website has changed, but our commitment to you never will.', url: '#'}
   	]
   end
 
